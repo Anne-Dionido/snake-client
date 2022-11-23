e21609fb3e1c7d50a727f5be097af165c0f989c1
Anne-Dionido <anne.dionido@telus.com>
2022-11-23 16:02:27 -0700

    adding updates

--- play.js
+++ play.js
@@ -2,3 +2,12 @@ const { connect } = require("./client.js");
 
 console.log("Connecting ...");
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
